730e2e47773043e2165e3793c56eb3b95be02d9f
AslamHamdi <wokle08@gmail.com>
2022-06-28 00:06:45 +0800

    update for deployment

--- Config/db.js
+++ Config/db.js
@@ -1,6 +1,7 @@
 require('dotenv').config({path: __dirname + '../../.env'})
 const mysql = require('mysql2')
 
+// Developmnet DB Config
 const pool = mysql.createPool({
     multipleStatements: true, 
     host: process.env.DB_HOST,
@@ -8,5 +9,15 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
 })
+
+
+// Production DB config
+// const pool = mysql.createPool({
+//     multipleStatements: true, 
+//     host: process.env.DB_HOST_PROD,
+//     user: process.env.DB_USER_PROD,
+//     database: process.env.DB_NAME_PROD,
+//     password: process.env.DB_PASSWORD_PROD,
+// })
 
 module.exports = pool.promise();
